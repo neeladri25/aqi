a1b0e91433d80d074f0fa653d7db3b74bc67318e
Neeladri Parikha <neeladrip1998@gmail.com>
2024-10-24 16:58:15 +0530

    Added @CrossOrigin to support API integration in UI

--- aqi/src/main/java/com/aqi/aqi/controller/AirQualityIndexController.java
+++ aqi/src/main/java/com/aqi/aqi/controller/AirQualityIndexController.java
@@ -4,11 +4,9 @@ import com.aqi.aqi.dto.AirQualityIndexResponseDto;
 import com.aqi.aqi.service.AirQualityIndexService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/air-quality-index/")
 public class AirQualityIndexController {
